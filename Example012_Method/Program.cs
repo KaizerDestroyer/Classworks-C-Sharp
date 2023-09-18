1f364f7105391fe470b23b19494d3e9d372c3a65
Karim <karimissanovts@gmail.com>
2023-09-18 19:39:34 +0600

    edit Example012 Methods

--- Example012_Method/Program.cs
+++ Example012_Method/Program.cs
@@ -14,16 +14,87 @@
 
 // Method2("Кикашки");
 
-void Method21(string message, int count)
+// void Method21(string message, int count)
+// {
+//     int i = 0;
+//     int num = 1;
+//     while (i < count)
+//     {
+//         Console.WriteLine(message + num);
+//         num++;
+//         i++;
+//     }
+// }
+
+// Method21("Кикашки", 100);
+
+
+// Method 3
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+// Method 4
+
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "la");
+// Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+int[] arr = { 1, 5, 6, 3, 2, 9, 4, 7, 8, 1, 3 };
+
+void PrintArray(int[] array)
 {
-    int i = 0;
-    int num = 1;
-    while (i < count)
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
     {
-        Console.WriteLine(message + num);
-        num++;
-        i++;
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j <= array.Length - 1; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
     }
 }
 
-Method21("Кикашки", 100);
+
+PrintArray(arr);
+SelectionSort(arr);
+
+
+PrintArray(arr);
